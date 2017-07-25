defeba31437a9961ada3efe290c3cb6a030ad9b9
MOe-13 <moetainment13@gmail.com>
2017-07-25 12:38:46 -0400

    Final

---
+++ exam/client/js/controllers.js
@@ -0,0 +1,52 @@
+//Hello There friend, I need your help. Please write out the functionallity to get, write, update, and delete data. I know some of the routes have been written out. 
+//Deleting and Updating should be by ID and after, I should be showing the get page. mainCtrl should also be empty. 
+//To give you a hint, you have to make HTTP calls and I believe Angular has a module for that...I don't remember. 
+//Please do not adjust the names of the controllers and the angular module. 
+
+
+angular.module("examApp.controllers", [])
+    .controller("mainCtrl", function($scope){})
+
+    .controller("getCtrl", function($scope, $http){
+    	$http.get("http://localhost:8080/api/data")
+    	.then(function(response){
+            $scope.post = response.data
+    	})
+    })
+    .controller("postCtrl", function($scope, $http, $window){
+    	$scope.createPost = function(){
+    	$http.post('/api/post', $scope.post, { headers: { 'Content-Type': 'application/json' } })
+    	.then(function(response){
+            $window.location.href = '/'
+    			$scope.msg = "dope!";
+    		console.log("dope")
+		}, function(response){ 
+			$scope.msg ="Not dope!"
+			console.log("not dope")
+		})
+    }
+    	})
+    	
+
+    .controller("updateCtrl", function($scope, $http, $window, $location){
+        var p = $location.search().p
+
+        $scope.deletePost = function(){
+            $http.delete('api/delete/:id', {params :{id: p}})
+            .then(function(response){
+                console.log("delete success")
+                $window.location.href='/'
+            })
+        }
+
+        $scope.editPost = function(){
+            $http.put('/api/update/:id', $scope.post, {params :{id: p}})
+            .then(function(response){
+                $window.location.href='/'
+                console.log("coolio")
+            }), function(response){
+                console.log("fail")
+            }
+        }
+
+    })
---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var path = require("path");
+var app = express();
+var mongoose = require("mongoose")
+//I need the body-parser and routes module
+var bodyParser = require("body-parser");
+var routes = require("./exam/routes/routes");
+
+app.use("/client", express.static(path.join(__dirname, "exam/client")));
+app.use("/templates", express.static(path.join(__dirname, "exam/client/templates")));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+
+//I can't seem to direct to my routes but I am running, can you link me to my routes? Please input the routes in this file. joesay..
+routes(app)
+
+app.listen(8080)
+mongoose.connect("mongodb://localhost/exam"); //What Port should I listen on? Please put the port in between the parantheses
